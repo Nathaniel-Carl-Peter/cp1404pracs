412c755ffb1fb3aad4ec234ac4e688c903f12c97
Nathaniel-Carl-Peter <nathanielcarl.peter@my.jcu.edu.au>
2023-09-24 20:33:28 +1000

    "Score menu: Valid score function complete"

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -1,14 +1,48 @@
-MENU = "Menu:\n(H)ello\n(G)oodbye\n(Q)uit"
-name = input("Enter name: ")
-print(MENU)
-choice = input(">>> ").upper()
-while choice != 'Q':
-    if choice == 'H':
-        print("Hello", name)
-    elif choice == 'G':
-        print("Goodbye", name)
-    else:
-        print("Invalid choice :P")
+"""
+CP1404 - Score Menu
+
+Pseudo:
+
+display menu
+get user input
+
+"""
+
+
+def main():
+    """Score Menu function"""
+    MENU = "Menu:\n(V)alid score\n(S)tars\n(Q)uit"
     print(MENU)
     choice = input(">>> ").upper()
-print("Finished. ;)")
+    while choice != 'Q':
+        if choice == 'V':
+            score = get_valid_score()
+            determined_score(score)
+        elif choice == 'S':
+
+        else:
+            print("Invalid choice :P")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Farewell friend. ;)")
+
+
+def get_valid_score():
+    """Get a valid score between 0 - 100"""
+    score = float(input("Enter score: "))
+    return score
+
+
+def determined_score(score):
+    """Function determined score"""
+    if score < 0 or score > 100:
+        print("Invalid score")
+    elif score >= 90:
+        print("Excellent")
+    elif score >= 50:
+        print("Passable")
+    else:
+        print("Bad")
+
+
+main()
