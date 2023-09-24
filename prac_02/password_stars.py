26d5638919cc035846c21838733483ecc6604987
Nathaniel-Carl-Peter <nathanielcarl.peter@my.jcu.edu.au>
2023-09-24 21:00:12 +1000

    password stars: complete

---
+++ prac_02/password_stars.py
@@ -0,0 +1,36 @@
+"""
+CP1404 - Programing 2
+
+Password Stars
+"""
+
+MINIMUM_LENGTH = 4
+
+
+def version_1():
+    """Get a password of valid size and print asterisks."""
+    password = input(f"Enter password of at least {MINIMUM_LENGTH} characters: ")
+    while len(password) < MINIMUM_LENGTH:
+        password = input(f"Enter password of at least {MINIMUM_LENGTH} characters: ")
+    print('*' * len(password))
+
+
+def main():
+    password = get_password(MINIMUM_LENGTH)
+    print_asterisk(password)
+
+
+def get_password(minimum_length):
+    password = input(f"Enter password of at least {minimum_length} characters: ")
+    while len(password) < minimum_length:
+        print("Password to short")
+        password = input(f"Enter password of at least {minimum_length} characters: ")
+    return password
+
+
+def print_asterisk(sequence):
+    print('*' * len(sequence))
+
+
+# version_1()
+main()
