e826b71e774505d09e9e43e2fc15189e4f5c7973
Nathaniel-Carl-Peter <nathanielcarl.peter@my.jcu.edu.au>
2023-10-01 23:24:56 +1000

    random complete

--- prac_03/randoms.py
+++ prac_03/randoms.py
@@ -2,13 +2,14 @@
 CP1404 - Random Numbers
 Nathaniel Carl Peter
 
-Pseudo:
-
 """
 
 import random
 
-
 print(random.randint(5, 20))  # line 1
+# line 1 results display random numbers in range from 5 to 20
 print(random.randrange(3, 10, 2))  # line 2
+# line 2 prints random numbers from 3
 print(random.uniform(2.5, 5.5))  # line 3
+# line 3 print random decimal numbers in range from 2.5 to 4.5
+# given the fact in python we count from 0.
