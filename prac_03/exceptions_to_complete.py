ec0e93bcbe8b35acb6911ee30c6614b02e9765c2
Nathaniel-Carl-Peter <nathanielcarl.peter@my.jcu.edu.au>
2023-10-01 22:36:52 +1000

    exceptions complete

---
+++ prac_03/exceptions_to_complete.py
@@ -0,0 +1,13 @@
+"""
+CP1404/CP5632 - Practical
+Fill in the TODOs to complete the task
+"""
+
+is_finished = False
+while not is_finished:
+    try:
+        result = int(input("Valid int: "))
+        is_finished = False
+    except ValueError: # TODO - add the exception you want to catch after except
+        print("Enter valid integer.")
+print(f"Valid result is {result}")
