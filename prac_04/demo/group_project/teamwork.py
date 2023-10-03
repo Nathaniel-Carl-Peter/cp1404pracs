4e83c04625d5596d73374b484fe11764b628d6ac
Nathaniel-Carl-Peter <nathanielcarl.peter@my.jcu.edu.au>
2023-10-03 20:12:22 +1000

    group project finally uploaded

---
+++ prac_04/demo/group_project/teamwork.py
@@ -0,0 +1,26 @@
+def main():
+    numbers = get_numbers()
+    square_numbers(numbers)
+    display_number(numbers)
+
+
+def get_numbers():
+    numbers = int(input(f"Enter numbers separated by commas: "))
+    values = numbers.split(' , ')
+    return numbers
+
+
+def square_numbers(number):
+
+
+
+
+def display_number(numbers):
+    """Display numbers"""
+
+
+
+
+main()
+
+# :D
