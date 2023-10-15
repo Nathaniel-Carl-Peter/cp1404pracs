0433536f4cd63a4a85bc6f4a13d278e3b0e68770
Nathaniel-Carl-Peter <nathanielcarl.peter@my.jcu.edu.au>
2023-10-15 22:53:31 +1000

    Colours complete

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,14 @@
+COLOUR_CODES = {"Absolute Zero": "#0048ba",
+                "Acid Green": "#b0bf1a",
+                "AliceBlue": "#f0f8ff",
+                "Alizarin crimson": "#e32636",
+                "Amber": "#ffbf00",
+                "Apricot": "#fbceb1",
+                "Aqua": "#00ffff",
+                "Aquamarine1": "#7fffd4",
+                "Azure1": "#f0ffff",
+                "Blue Violet": "#8a2be2"}
+colour_name = input("Enter colour name: ")
+while colour_name != "":
+    print(f" The code for {colour_name} is {COLOUR_CODES.get(colour_name)}")
+    colour_name = input("Enter colour name: ")
