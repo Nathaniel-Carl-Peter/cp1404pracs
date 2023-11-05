111d1ceedc2a3565ea66df6b0ab2769a08d63913
Nathaniel-Carl-Peter <nathanielcarl.peter@my.jcu.edu.au>
2023-11-05 18:05:24 +1000

    prac 07 languages complete

---
+++ prac_07/programming_language.py
@@ -0,0 +1,47 @@
+"""
+CP1404/CP5632 Practical - Suggested Solution
+Programming Language class with tests.
+"""
+
+
+class ProgrammingLanguage:
+    """Represent information about a programming language."""
+
+    def __init__(self, name, typing, reflection, year, pointer_arithmetic):
+        """Construct a ProgrammingLanguage from the given values."""
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+        self.pointer_arithmetic = pointer_arithmetic
+
+    def __str__(self):
+        return f"{self.name}, Typing{self.typing}, Reflection{self.reflection}," \
+               f"Pointer Arithmetic={self.pointer_arithmetic}, First Appeared {self.year}"
+
+    def __repr__(self):
+        """Return string representation of a ProgrammingLanguage."""
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
+
+    def is_dynamic(self):
+        """Determine if language is dynamically typed."""
+        return self.typing == "Dynamic"
+
+
+def run_tests():
+    """Run simple tests/demos on ProgrammingLanguage class."""
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, False, 1995)
+    python = ProgrammingLanguage("Python", "Dynamic", True, False, 1991)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, False, 1991)
+    smalltalk = ProgrammingLanguage("Smalltalk ", "Static", False, False, 1972)
+    languages = [ruby, python, visual_basic, smalltalk]
+    print(python)
+
+    print("The dynamically typed languages are:")
+    for language in languages:
+        if language.is_dynamic():
+            print(language.name)
+
+
+if __name__ == "__main__":
+    run_tests()
