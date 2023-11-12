b93cb159ef0cc952d9769ff57bdc683106757adb
Nathaniel-Carl-Peter <nathanielcarl.peter@my.jcu.edu.au>
2023-11-12 23:24:24 +1000

    Squaring complete

---
+++ prac_08/squaring/squaring.py
@@ -0,0 +1,30 @@
+"""
+Cp1404 - Practical
+Nathaniel Carl Peter
+"""
+
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.core.window import Window
+
+
+class SquareNumberApp(App):
+    """KV app square numbers"""
+
+    def build(self):
+        """Build app base from kv file"""
+        Window.size = (500, 500)
+        self.title = 'Square Numbers'
+        self.root = Builder.load_file('squaring.kv')
+        return self.root
+
+    def handle_calculation(self, value):
+        """Handle calculations"""
+        try:
+            results = int(value) ** 2
+            self.root.ids.output_label = str(results)
+        except ValueError:
+            self.root.ids.output_label.txt = ""
+
+
+SquareNumberApp().run()
